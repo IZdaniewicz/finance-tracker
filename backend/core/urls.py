1d5385343d7fe69008182719392e62e85d0af149
kmaliszewski <kuba.maliszewski01@gmail.com>
2023-05-02 14:01:00 +0200

    add unit test for transaction and account

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 
 from . import views
+
 urlpatterns = [
-    path("account/<int:account_id>", views.GetAccountById),
 
-    path("account", views.AccountAPIView.as_view()),
-    path("transaction", views.TransactionAPIView.as_view()),
+    path("account", views.AccountAPIView.as_view(), name='account'),
+    path("account/<int:account_id>", views.GetAccountById, name='account-detail'),
+    path("transaction", views.TransactionAPIView.as_view(), name='transaction'),
+    path("transaction/<int:transaction_id>", views.GetTransactionById, name='transaction-detail'),
 
-    path("transaction/<int:transaction_id>", views.GetTransactionById),
     path("transaction/day/<int:day>", views.GetAllTransactionByDay),
     path("transaction/month/<int:month>", views.GetAllTransactionByMonth),
     path("account/transaction/<int:account_id>", views.GetAllTransactionByAccountId),
 ]
-
